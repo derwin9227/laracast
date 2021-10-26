5c0a6c4fedcb60d831f3ff58b5537224bfa8569b
derwin <derwin9227@gmail.com>
2021-10-26 08:03:32 -0500

    Store blog posts as html files

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+
+    <body>
+        
+        <article>
+            
+          <?= $post;?>
+
+        </article>
+
+        <a href="/" >Go Back</a>
+      
+    </body>
+
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <title>laravel</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+
+    <body>
+        
+        <article>
+            <a href="/posts/my-first-post"> 
+                <h1>First post</h1>
+            </a>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit similique nulla, culpa, eius, a est rem ducimus dolores doloremque dolore corrupti quam? Impedit, repellat? Quis repudiandae veniam perspiciatis voluptas.
+            </p>
+
+        </article>
+
+        <article>
+            <a href="/posts/my-second-post"> 
+                <h1>Second post</h1>
+            </a>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit similique nulla, culpa, eius, a est rem ducimus dolores doloremque dolore corrupti quam? Impedit, repellat? Quis repudiandae veniam perspiciatis voluptas.
+            </p>
+
+        </article>
+
+        <article>
+            <a href="/posts/my-third-post"> 
+                <h1>Third post</h1>
+            </a>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit similique nulla, culpa, eius, a est rem ducimus dolores doloremque dolore corrupti quam? Impedit, repellat? Quis repudiandae veniam perspiciatis voluptas.
+            </p>
+
+        </article>
+
+        
+    </body>
+
+</html>
