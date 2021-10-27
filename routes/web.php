cb2e6e473dcdee68fb07845bec259e1f5921993a
derwin <derwin9227@gmail.com>
2021-10-27 08:12:08 -0500

    usando cache

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,16 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    
 
-    ddd($path);
-
-    if(! file_exists($path)){
+    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 5/* segundos o now()->addminutes(10) */, fn() => file_get_contents($path));
 
     return view('post', [
         'post' => $post
     ]);
-})->where('post', '[A-z]+');
+})->where('post', '[A-z_\-]+');
 //whereAlpha('post');
