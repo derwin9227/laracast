5dd0f313fcf4240dfc701a5c75f301e827f9cc3e
derwin <derwin9227@gmail.com>
2021-10-28 08:06:46 -0500

    use the filesystem class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,41 +7,11 @@
     </head>
 
     <body>
-        
-        <article>
-            <a href="/posts/my-first-post"> 
-                <h1>First post</h1>
-            </a>
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit similique nulla, culpa, eius, a est rem ducimus dolores doloremque dolore corrupti quam? Impedit, repellat? Quis repudiandae veniam perspiciatis voluptas.
-            </p>
-
-        </article>
-
-        <article>
-            <a href="/posts/my-second-post"> 
-                <h1>Second post</h1>
-            </a>
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit similique nulla, culpa, eius, a est rem ducimus dolores doloremque dolore corrupti quam? Impedit, repellat? Quis repudiandae veniam perspiciatis voluptas.
-            </p>
-
-        </article>
-
-        <article>
-            <a href="/posts/my-third-post"> 
-                <h1>Third post</h1>
-            </a>
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit similique nulla, culpa, eius, a est rem ducimus dolores doloremque dolore corrupti quam? Impedit, repellat? Quis repudiandae veniam perspiciatis voluptas.
-            </p>
-
-        </article>
-
-        
+        <?php foreach ($posts as $post) : ?>
+            <article>
+                <?php $post; ?>
+            </article>
+        <?php endforeach; ?>
     </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Post;
+
 Route::get('/', function () {
-    return view('posts');
+
+    return view('posts',[
+        'posts' => Post::all()
+    ]);
+
 });
 
 Route::get('posts/{post}', function($slug) {
-    
 
-    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", 5/* segundos o now()->addminutes(10) */, fn() => file_get_contents($path));
-
-    return view('post', [
-        'post' => $post
+    return view('post',[
+        'post' => Post::find($slug)
     ]);
+
 })->where('post', '[A-z_\-]+');
-//whereAlpha('post');
