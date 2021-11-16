30c71c3c8c8dbf4cb8dd062346c43494dfbbc311
derwin <derwin9227@gmail.com>
2021-11-16 08:05:22 -0500

    fix a confusing eloquent query bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,11 +32,12 @@ class Post extends Model
 
     }//filter */
     public function scopeFilter($query, array $filters){
+
         $query->when($filters['search'] ?? false, fn($query, $search) =>
-        
-            $query
-                ->where('title', 'like', '%' . request('search') . '%')
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . request('search') . '%')
                 ->orWhere('body', 'like', '%' . request('search') . '%')
+                )
             );
 
         $query->when($filters['category'] ?? false, fn($query, $category) =>
