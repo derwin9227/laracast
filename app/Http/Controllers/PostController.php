1e0bf35b30fe235088916e054a5c437d21dca805
derwin <derwin9227@gmail.com>
2021-11-17 08:03:46 -0500

    laughably simple pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,9 +11,11 @@ class PostController extends Controller
     public function index(){
     
         return view('posts.index',[
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
+            'posts' => Post::latest()
+            ->filter(request(['search', 'category', 'author']))
+            ->paginate(3)->withQueryString(),
         ]);
-    }//index
+    }//index 
 
     public function show(Post $post){
 
