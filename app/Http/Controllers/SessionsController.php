6eec02ccf5c5eb20a61ce07e69315c1ce57b8361
derwin <derwin9227@gmail.com>
2021-11-20 10:53:01 -0500

    buld the log in page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }//create
+
+    public function store(){
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes)){
+            session()->regenerate();
+            return redirect('/')->with('success', 'welcome Back');
+        }//if
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials coud not be verified.'
+        ]);
+        /* return back()
+        ->withInput()
+        ->withErrors(['email' => 'Your provided credentials coud not be verified.']); */
+    }//store
+
     public function destroy(){
         auth()->logout();
 
         return redirect('/')->with('success', 'Goodbye!');
-    }
+    }//logout
 }
