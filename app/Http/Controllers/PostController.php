b8b2dfda4ca843b82378b86f9bd7336fcb8fb545
derwin <derwin9227@gmail.com>
2021-12-03 09:30:33 -0500

    create the publish form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
@@ -28,5 +29,23 @@ class PostController extends Controller
         return view('posts.create');
 
     }//create
+
+    public function store(){
+        
+        $attributes = request()->validate([
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => 'required',
+            'body' => 'required',
+            //'category_id' => 'required|unique:categories,id',
+            'category_id' => ['required' , Rule::exists('categories', 'id')]
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        Post::create($attributes);
+
+        return redirect('/');
+    }//store
 
 }
