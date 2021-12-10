c6f19e644406c6134d292bc1f0826459166b5b7e
derwin <derwin9227@gmail.com>
2021-12-10 07:59:02 -0500

    all about authorization

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Str;
@@ -12,7 +13,7 @@ use Illuminate\Support\Str;
 class PostController extends Controller
 {
     public function index(){
-
+        //dd(Gate::allows('admin'));
         return view('posts.index',[
             'posts' => Post::latest()
             ->filter(request(['search', 'category', 'author']))
